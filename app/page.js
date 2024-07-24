fdcd6677c9ee0afc68ed86c769930341b3f27b8f
Heru <Heru.Wang@edu.sait.ca>
2024-07-24 14:24:31 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
-import Image from "next/image";
+"use client";
+
 import Comments from "./comment-area/comments";
 import Link from "next/link";
+import GameDetail from "./components/GameDetail";
 
-//this page is used for testing purposes, feel free to render your components here
+// This page is used for testing purposes, feel free to render your components here
 export default function Home() {
+  const title = "hades"; // This is the title of the game user want to search for
+
   return (
     <main>
       <h1>Final Project Template</h1>
       <Link href="/Home">Go to home page</Link>
+      <GameDetail title={title} /> 
       <Comments />
     </main>
   );
