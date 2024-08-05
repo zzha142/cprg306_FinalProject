54df393257629c1e860e2bcfaaa228d0bf9473b7
zzha142 <zzha142@outlook.com>
2024-08-04 19:20:06 -0600

    aadd Suspense for useSearchParams()

--- app/gameDetail/page.js
+++ app/gameDetail/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+import { Suspense } from "react";
 import Comments from "../components/comments";
 import Link from "next/link";
 import GameDetail from "../components/GameDetail";
@@ -11,21 +11,22 @@ export default function GameDetailPage() {
   const searchParams = useSearchParams();
   const title = searchParams.get("title") || "";
   const titleStyle = "text-4xl font-bold text-center pt-5 pb-5";
-
   return (
-    <main className="relative p-4">
-      <header className="relative">
-        <Link
-          href="/"
-          className="absolute top-4 left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded z-10"
-        >
-          Back to Home
-        </Link>
-        <h1 className={titleStyle}>Share reviews!</h1>
-        <Login />
-      </header>
-      <GameDetail initialTitle={title} />
-      <Comments currentGameTitle={title} />
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="relative p-4">
+        <header className="relative">
+          <Link
+            href="/"
+            className="absolute top-4 left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded z-10"
+          >
+            Back to Home
+          </Link>
+          <h1 className={titleStyle}>Share reviews!</h1>
+          <Login />
+        </header>
+        <GameDetail initialTitle={title} />
+        <Comments currentGameTitle={title} />
+      </main>
+    </Suspense>
   );
 }
