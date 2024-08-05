13df8bc6d85ad19e62a7b2320466216af82ae912
zzha142 <zzha142@outlook.com>
2024-08-04 19:24:08 -0600

    make adjustment for suspense

--- app/gameDetail/page.js
+++ app/gameDetail/page.js
@@ -7,26 +7,33 @@ import Login from "../components/login";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
-export default function GameDetailPage() {
+function GameDetailContent() {
   const searchParams = useSearchParams();
   const title = searchParams.get("title") || "";
   const titleStyle = "text-4xl font-bold text-center pt-5 pb-5";
+
+  return (
+    <main className="relative p-4">
+      <header className="relative">
+        <Link
+          href="/"
+          className="absolute top-4 left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded z-10"
+        >
+          Back to Home
+        </Link>
+        <h1 className={titleStyle}>Share reviews!</h1>
+        <Login />
+      </header>
+      <GameDetail initialTitle={title} />
+      <Comments currentGameTitle={title} />
+    </main>
+  );
+}
+
+export default function GameDetailPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <main className="relative p-4">
-        <header className="relative">
-          <Link
-            href="/"
-            className="absolute top-4 left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded z-10"
-          >
-            Back to Home
-          </Link>
-          <h1 className={titleStyle}>Share reviews!</h1>
-          <Login />
-        </header>
-        <GameDetail initialTitle={title} />
-        <Comments currentGameTitle={title} />
-      </main>
+      <GameDetailContent />
     </Suspense>
   );
 }
